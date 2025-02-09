e5507034bcf67aa736bf558f8cdc3c2a6bcb0cc5
Pratik059 <72210275+Pratik059@users.noreply.github.com>
2025-02-09 21:35:26 +0530

    form validation added

--- basic-form/src/components/Frame.jsx
+++ basic-form/src/components/Frame.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputCard from './InputCard'
 
-function Submit(){
+function Submit({submitfunction}){
+
   return (
-    <button>
+    <button onClick={submitfunction}>
       <div className='w-[350px] h-12 flex justify-center items-center bg-green-500 hover:cursor-pointer
       hover:bg-green-400'>
         <p className='text-white'>Submit</p>
@@ -13,12 +14,67 @@ function Submit(){
 }
 
 function Frame() {
+
+  const errorMessages = {
+    emptyError: 'This field cannot be empty',
+    emailError: 'Please enter a valid email'
+  }
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  })
+  const [errors, setErrors] = useState({})
+
+  const onInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+
+    setErrors({
+      ...errors,
+      [e.target.name]: ''
+    })
+    // console.log(formData)
+  }
+  
+  const validateForm = () => {
+    let newErrors = {}
+  
+    if(!formData.firstName.trim()){
+      newErrors.firstName = errorMessages.emptyError
+    }
+    if(!formData.lastName.trim()){
+      newErrors.lastName = errorMessages.emptyError
+    }
+    if(!formData.email.trim()){
+      newErrors.email = errorMessages.emptyError
+    }
+    else if(!/\S+@\S+\.\S+/.test(formData.email)){
+      newErrors.email = errorMessages.emailError
+    }
+    setErrors(newErrors)
+    return Object.keys(newErrors).length === 0;
+  }
+
+  const SubmitForm = () => {
+    if(validateForm()){
+      alert('Form Submitted Successfully')
+    }
+    else {
+      alert('Please fix the errors before submitting')
+    }
+  }
+
+
   return (
     <div className='w-[400px] h-[300px] bg-white flex flex-col justify-evenly items-center shadow-xl shadow-black'>
-        <InputCard field='First Name'/>
-        <InputCard field='Last Name'/>
-        <InputCard field='Email'/>
-        <Submit />
+        <InputCard field='First Name' name="firstName" onInputChange={onInputChange} error={errors.firstName}/>
+        <InputCard field='Last Name' name="lastName" onInputChange={onInputChange} error={errors.lastName}/>
+        <InputCard field='Email' name="email" onInputChange={onInputChange} error={errors.email}/>
+        <Submit submitfunction = {SubmitForm}/>
     </div>
   )
 }
--- basic-form/src/components/InputCard.jsx
+++ basic-form/src/components/InputCard.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 
 function InputCard(props) {
 
-    const { field } = props
+    const { field, name, onInputChange, error } = props
 
   return (
-        <input className="
-        focus:border-0 focus:outline-0
-          w-[350px] rounded-sm h-12
-          px-5
-          bg-blue-100
-        " 
-        type="text" 
-        placeholder={field}
-        />
+        <div>
+
+          <input className="
+          focus:border-0 focus:outline-0
+            w-[350px] rounded-sm h-12
+            px-5
+            bg-blue-100
+          " 
+          type="text"
+          name={name}
+          onChange={onInputChange} 
+          placeholder={field}
+          />
+          {error && <p>{error}</p>}
+        </div>
+        
   )
 }
 
